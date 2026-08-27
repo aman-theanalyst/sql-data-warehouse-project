849cfe2256025c212acf154fc06720c814abcfd8
Aman Singh Negi <theanalyst.aman@gmail.com>
2026-08-27 15:24:37 +0530

    Delete tests/01_Checking_data & Drawing_Relationships.sql

--- tests/01_Checking_data & Drawing_Relationships.sql
+++
@@ -1,9 +0,0 @@
--- Checking the Data and creating relationships between tables
-
-SELECT * FROM bronze.crm_sales_details LIMIT 1000;
-SELECT * FROM bronze.erp_cust_az12 LIMIT 1000;
-SELECT * FROM bronze.crm_cust_info LIMIT 1000;
-
-SELECT * FROM bronze.erp_loc_a101 LIMIT 1000;
-SELECT * FROM bronze.erp_px_cat_g1v2 LIMIT 1000;
-SELECT * FROM bronze.crm_prd_info LIMIT 1000;
